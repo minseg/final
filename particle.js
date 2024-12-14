c50a2a946f0d42e414c781431484d225860a9532
minseg <artssam7602@naver.com>
2024-12-14 20:57:56 +0900

    꽃잎 입자클래스 작성

---
+++ particle.js
@@ -0,0 +1,24 @@
+class Particle {
+    constructor(position) {
+        this.pos = position.copy();
+        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.acc = createVector(0, 0);
+        this.size = random(20, 40); // 입자 크기
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+    }
+
+    show() {
+        noStroke();
+        fill(150, 100, 255, 150);
+        ellipse(this.pos.x, this.pos.y, this.size);
+    }
+}
