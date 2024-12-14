34a98a5609466ebd27e2c352fff42d7ae169fcea
minseg <artssam7602@naver.com>
2024-12-14 21:12:04 +0900

    꽃잎이 가라앉는 효과 추가

--- particle.js
+++ particle.js
@@ -5,16 +5,32 @@ class Particle {
         this.acc = createVector(0, 0);
         this.size = random(20, 40); // 입자 크기
         this.angle = random(TWO_PI); // 입자의 회전 각도
+        this.settled = false; // 가라앉음 여부
     }
 
     applyForce(force) {
-        this.acc.add(force);
+        if (!this.settled) {
+            this.acc.add(force);
+        }
     }
 
     update() {
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
+        if (!this.settled) {
+            this.vel.add(this.acc);
+            this.pos.add(this.vel);
+            this.acc.mult(0);
+
+            // 위쪽 경계에 닿으면 가라앉기 시작
+            if (this.pos.y <= 50) {
+                this.vel = createVector(0, random(1, 3)); // 아래로 느리게 떨어짐
+            }
+
+            // 바닥에 닿으면 정지
+            if (this.pos.y >= height) {
+                this.settled = true;
+                this.vel.mult(0);
+            }
+        }
     }
 
     show() {
@@ -37,5 +53,4 @@ class Particle {
         endShape(CLOSE);
     }
 }
-
 
