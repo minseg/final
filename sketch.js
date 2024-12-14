10a749a2b7641e783a5f30a5378c7c32370f5a42
minseg <artssam7602@naver.com>
2024-12-14 21:13:34 +0900

    바람세기에 따라 줄기색상 변화 구현

--- sketch.js
+++ sketch.js
@@ -1,25 +1,24 @@
-let particleSystem;
-let repeller, attractor;
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-
-    // 입자 시스템 초기화
-    particleSystem = new ParticleSystem(createVector(width / 6, height - 300));
-}
-
 function draw() {
-    background(240);
+  background(240);
 
-    // 줄기와 꽃봉우리
-    stroke(50, 200, 50);
-    strokeWeight(6);
-    line(width / 6, height, width / 6, height - 300);
+  // Repeller와 Attractor의 힘에 따라 줄기의 색상 변경
+  let windForce = repeller.strength + attractor.strength;
+  let stemColor = map(abs(windForce), 0, 100, 50, 255);
 
-    noStroke();
-    fill(255, 100, 150);
-    ellipse(width / 6, height - 300, 50, 50);
+  // 줄기 그리기
+  stroke(50, stemColor, 50); // 바람에 따라 녹색 농도 변화
+  strokeWeight(6);
+  line(width / 6, height, width / 6, height - 300);
 
-    // 입자 시스템 실행
-    particleSystem.run();
+  // 꽃봉우리 그리기
+  noStroke();
+  fill(255, 100, 150);
+  ellipse(width / 6, height - 300, 50, 50);
+
+  // 입자 시스템 실행
+  particleSystem.run();
+
+  // Repeller와 Attractor 표시
+  repeller.show();
+  attractor.show();
 }
