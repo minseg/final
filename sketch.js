bb24522dab897a358d4b44ad57ce432b53ee3e8d
minseg <artssam7602@naver.com>
2024-12-14 20:57:21 +0900

    줄기랑 꽃봉우리 추가

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(240);
+
+  // 줄기 그리기
+  stroke(50, 200, 50);
+  strokeWeight(6);
+  line(width / 6, height, width / 6, height - 300);
+
+  // 꽃봉우리 그리기
+  noStroke();
+  fill(255, 100, 150);
+  ellipse(width / 6, height - 300, 50, 50);
 }
