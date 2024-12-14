6d5f743e3ec73113a26b22e162decbf0d3ca9ec3
minseg <artssam7602@naver.com>
2024-12-14 21:38:39 +0900

    원 상호작용 조정

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let particleSystem;
 let repeller, attractor;
 let currentColor; // 현재 색상 (꽃잎, 봉우리)
+let draggingRepeller = false; // Repeller 드래그 여부
+let draggingAttractor = false; // Attractor 드래그 여부
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -28,6 +30,10 @@ function draw() {
     // 입자 시스템 실행
     particleSystem.applyRepeller(repeller);
     particleSystem.applyAttractor(attractor);
+
+    // Repeller와 Attractor가 서로 상호작용
+    interactRepellerAndAttractor();
+
     particleSystem.run();
 
     // 모든 입자가 바닥에 가라앉으면 새로운 꽃잎 생성
@@ -38,6 +44,43 @@ function draw() {
     // Repeller와 Attractor 표시
     repeller.show();
     attractor.show();
+}
+
+// 두 원의 상호작용 구현
+function interactRepellerAndAttractor() {
+    let distance = dist(repeller.pos.x, repeller.pos.y, attractor.pos.x, attractor.pos.y);
+
+    // 상호작용 로직 (거리 기반으로 힘 조정)
+    if (distance < 200) {
+        let force = map(distance, 0, 200, 50, 0); // 가까울수록 힘 증가
+        let dir = repeller.pos.copy().sub(attractor.pos).normalize().mult(force);
+        repeller.pos.add(dir);
+        attractor.pos.sub(dir);
+    }
+}
+
+// 마우스 클릭한 원 판별
+function mousePressed() {
+    if (dist(mouseX, mouseY, repeller.pos.x, repeller.pos.y) < repeller.size / 2) {
+        draggingRepeller = true;
+    } else if (dist(mouseX, mouseY, attractor.pos.x, attractor.pos.y) < attractor.size / 2) {
+        draggingAttractor = true;
+    }
+}
+
+// 마우스 드래그로 원 이동
+function mouseDragged() {
+    if (draggingRepeller) {
+        repeller.pos.set(mouseX, mouseY);
+    } else if (draggingAttractor) {
+        attractor.pos.set(mouseX, mouseY);
+    }
+}
+
+// 마우스 놓으면 드래그 상태 해제
+function mouseReleased() {
+    draggingRepeller = false;
+    draggingAttractor = false;
 }
 
 // 키보드 입력으로 바람의 세기 조정
@@ -61,3 +104,4 @@ function keyPressed() {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
+
